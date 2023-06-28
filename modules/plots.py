c570e250921c22cda10528afd02c7a40f9a70406
PanosGidarakos <74772930+PanosGidarakos@users.noreply.github.com>
2023-06-28 13:07:40 +0200

    Update plots.py

--- modules/plots.py
+++ modules/plots.py
@@ -5,6 +5,15 @@ from datetime import datetime
 import seaborn as sns
 from dtw import dtw
 
+import stumpy
+from tqdm import tqdm
+import random
+import matplotlib
+matplotlib.rcParams.update({'font.size': 12,'figure.figsize': [20, 4],'lines.markeredgewidth': 0,
+                            'lines.markersize': 2})
+
+from modules.learning import clean_motifs,scorer
+
 def plot_knee(mps, save_plot=False, filename='knee.png'):
     
     """ 
@@ -229,3 +238,359 @@ def plot_segmentation(df, path, output, fixed_dates, file_name, top_seg=3):
             fig.savefig(name + "_" + str(i))
     model = pd.DataFrame.from_dict({"L": lam,'Changepoints indexes': regi, "Changepoints Dates": dloc, "Normalized Distance": (diff1-np.min(diff1))/(np.max(diff1) - np.min(diff1))})
     return best, model
+
+
+
+
+
+
+
+def motif_graph_multi_dim(col,df,df_soil_output,alles_df,n,plot=True):
+    """
+    Plots the best results returned by fitness function. Motifs are being plotted in a view a barplots
+    df: pandas dataframe
+    df_soil_output: pandas dataframe of soiling events
+    col: columns of the dataframe to perform the routine
+    alles_df: the dataframe returned by fitness fucntion
+    n: the number of outputs to plot
+    """
+    import sklearn.metrics
+    for p in range(alles_df.shape[0])[:n]:
+        
+
+
+        stumpy.config.STUMPY_EXCL_ZONE_DENOM= alles_df.iloc[p]['exclusion_zone']
+        mp,mpi=stumpy.mstump(df[col], m=int(alles_df.iloc[p]['profile_wind']),normalize=True)
+        md,mi,_,_=stumpy.mmotifs(df[col], mp, mpi,min_neighbors=alles_df.iloc[p]['min_nei'],
+                                 max_distance=alles_df.iloc[p]['max_d'],cutoffs=alles_df.iloc[p]['cutoff'],
+                                 max_matches=alles_df.iloc[p]['max_m'],max_motifs=alles_df.iloc[p]['max_motif'])  
+       
+        md,mi=clean_motifs(md,mi)
+        m=int(alles_df.iloc[p]['profile_wind'])
+        dates = pd.DataFrame(index=range(len(df)))
+        dates = dates.set_index(df.index)
+        dates['power']=df['power']        
+        dates['soil']=df.soiling_derate
+        for i,mtype in enumerate(mi):
+#             print(i,mtype)
+            if i==alles_df.iloc[p]['motif_type']-1:
+                test=pd.DataFrame()
+                test.index=df.index
+                test['pred']=np.nan
+                for j in mtype:
+                    test['pred'].iloc[j:j+m]=1
+                test['pred'] = test['pred'].fillna(0)
+                test["actual"] = np.nan
+                for start, end in zip(df_soil_output.SoilStart, df_soil_output.SoilStop):
+                    test.loc[start:end, 'actual'] = 1
+                test['actual'] = test['actual'].fillna(0)
+                f1,ps,recal,hamming,jaccard,cohen,roc=scorer(test)
+                if plot==True:
+                
+                    ax=df.soiling_derate.plot(color="green",label='Soil Derate',figsize=(20,10))
+                    ax.set_xlabel('Periods defined by best motif type',fontsize=24)
+                    ax.set_ylabel("Soil Derate",color="black",fontsize=24)
+                    for d in df_soil_output.SoilStart:
+                        ax.axvline(x=d, color='black', linestyle='-')
+                    ax.axvline(x=d, color='black', linestyle='-',label='Soil Start')
+                    for d in df_soil_output.SoilStop:
+                        ax.axvline(x=d, color='black', linestyle=':') 
+                    ax.axvline(x=d, color='black', linestyle=':',label='Soil Ends') 
+                    plt.legend(fontsize=14,loc='lower left')   
+                    for idx in mtype:
+                        ax.set_title(f'{m}-Days length,{i+1}-Motif Type, {len(mtype)}-Neighbors, F1:{np.round(f1,3)}',
+                                     fontsize=20)
+                        ax.axvspan(dates.index[idx],dates.index[idx+m], color = 'orange',ymax=0.96, alpha = 0.9)
+                    ax.axvspan(dates.index[idx],dates.index[idx+m], color = 'orange',ymax=0.96, alpha = 0.9,label='motifs')
+
+
+                    plt.legend(fontsize=14,loc='lower left')   
+                    plt.show()
+                print(sklearn.metrics.classification_report(test.actual,test.pred))
+                
+
+def motif_graph_multi_dim_eval(col,df,df_soil_output,alles_df,n,plot=False):
+    """
+    Plots the best results returned by fitness function. Motifs are being plotted in a view a barplots
+    df: pandas dataframe
+    df_soil_output: pandas dataframe of soiling events
+    col: columns of the dataframe to perform the routine
+    alles_df: the dataframe returned by fitness fucntion
+    n: the number of outputs to plot
+    """
+    import sklearn.metrics
+    for p in range(alles_df.shape[0])[:n]:
+        stumpy.config.STUMPY_EXCL_ZONE_DENOM=alles_df.iloc[p]['exclusion_zone']
+
+        
+
+
+       
+        mp,mpi=stumpy.mstump(df[col], m=int(alles_df.iloc[p]['profile_wind']),normalize=True)
+        md,mi,_,_=stumpy.mmotifs(df[col], mp, mpi,min_neighbors=alles_df.iloc[p]['min_nei'],
+                                 max_distance=alles_df.iloc[p]['max_d'],cutoffs=alles_df.iloc[p]['cutoff'],
+                                 max_matches=alles_df.iloc[p]['max_m'],max_motifs=alles_df.iloc[p]['max_motif'])  
+       
+        md,mi=clean_motifs(md,mi)
+        m=int(alles_df.iloc[p]['profile_wind'])
+        dates = pd.DataFrame(index=range(len(df)))
+        dates = dates.set_index(df.index)
+        dates['power']=df['power']        
+        dates['soil']=df.soiling_derate
+        for i,mtype in enumerate(mi):
+#             print(i,mtype)
+            if i==alles_df.iloc[p]['motif_type']-1:
+                test=pd.DataFrame()
+                test.index=df.index
+                test['pred']=np.nan
+                for j in mtype:
+                    test['pred'].iloc[j:j+m]=1
+                test['pred'] = test['pred'].fillna(0)
+                test["actual"] = np.nan
+                for start, end in zip(df_soil_output.SoilStart, df_soil_output.SoilStop):
+                    test.loc[start:end, 'actual'] = 1
+                test['actual'] = test['actual'].fillna(0)
+                f1,ps,recal,hamming,jaccard,cohen,roc=scorer(test)
+                if plot==True:
+                    ax=df.soiling_derate.plot(color="green",label='Soil Derate',figsize=(20,10))
+                    ax.set_xlabel('Periods defined by best motif type',fontsize=24)
+                    ax.set_ylabel("Soil Derate",color="black",fontsize=24)
+                    for d in df_soil_output.SoilStart:
+                        ax.axvline(x=d, color='black', linestyle='-')
+                    ax.axvline(x=d, color='black', linestyle='-',label='Soil Start')
+                    for di in df_soil_output.SoilStop:
+                        ax.axvline(x=di, color='black', linestyle=':') 
+                    ax.axvline(x=di, color='black', linestyle=':',label='Soil Ends') 
+                    plt.legend(fontsize=14,loc='lower left')   
+                    for idx in mtype:
+                        ax.set_title(f'{m}-Days length, {i+1}-Motif Type, {len(mtype)}-Neighbors, F1:{np.round(f1,3)}',
+                                     fontsize=20)
+                        ax.axvspan(dates.index[idx],dates.index[idx+m], color = 'orange',ymax=0.96, alpha = 0.9)
+                    ax.axvspan(dates.index[idx],dates.index[idx+m], color = 'orange',ymax=0.96, alpha = 0.9,label='motifs')
+
+
+                    plt.legend(fontsize=14,loc='lower left')
+
+                    plt.show()
+                print(sklearn.metrics.classification_report(test.actual,test.pred))
+
+
+
+
+
+
+
+def evaluate_motifs(col,df,df_soil_output,alles_df):
+    import sklearn.metrics
+    eval_df=pd.DataFrame()
+    eval_df.index=range(alles_df.shape[0])
+    score_list=[]
+    score_list_jac=[]
+    score_list_recall=[]
+    score_list_hamming=[]
+    score_list_pres=[]
+
+    type_list=[]
+    for p in range(alles_df.shape[0]):
+        stumpy.config.STUMPY_EXCL_ZONE_DENOM=alles_df.iloc[p]['exclusion_zone']
+#         print(alles_df.iloc[p])
+        mp,mpi=stumpy.mstump(df[col], m=int(alles_df.iloc[p]['profile_wind']),normalize=True)
+        md,mi,_,_=stumpy.mmotifs(df[col], mp, mpi,min_neighbors=alles_df.iloc[p]['min_nei'],
+                                 max_distance=alles_df.iloc[p]['max_d'],cutoffs=alles_df.iloc[p]['cutoff'],
+                                 max_matches=alles_df.iloc[p]['max_m'],max_motifs=alles_df.iloc[p]['max_motif'])  
+        md,mi=clean_motifs(md,mi)
+        m=int(alles_df.iloc[p]['profile_wind'])
+        
+        maxi=0
+        lista=[]
+        listam=[]
+        listjac=[]
+        listham=[]
+        listpre=[]
+        listrec=[]
+        for i,mtype in enumerate(mi):
+            test=pd.DataFrame()
+            test.index=df.index
+            test['actual']=np.nan
+            for start, end in zip(df_soil_output.SoilStart, df_soil_output.SoilStop):
+                test.loc[start:end, 'actual'] = 1
+            test['actual'] = test['actual'].fillna(0)
+            test['pred']=np.nan
+            for j in mtype:
+                test['pred'].iloc[j:j+m]=1
+            test['pred'] = test['pred'].fillna(0)
+            f1,ps,recal,hamming,jaccard,cohen,roc=scorer(test)
+            lista.append(f1)
+            listjac.append(jaccard)
+            listham.append(hamming)
+            listpre.append(ps)
+            listrec.append(recal)
+            listam.append(i)
+#             print(recal)
+            
+        score_list.append(np.max(lista))
+        
+        score_list_jac.append(listjac[lista.index(max(lista))])
+        score_list_recall.append(listrec[lista.index(max(lista))])
+        score_list_hamming.append(listham[lista.index(max(lista))])
+        score_list_pres.append(listpre[lista.index(max(lista))])
+        
+        type_list.append(listam[np.argmax(lista)]+1)
+        
+    
+    eval_df['f1']=score_list
+    eval_df['jaccard']=score_list_jac
+    eval_df['recall']=score_list_recall
+    eval_df['hamming']=score_list_hamming
+    eval_df['pres']=score_list_pres
+
+
+
+    eval_df['motif_type']=type_list
+    
+    return eval_df,score_list,type_list
+            
+    
+def matching_eval(col,df_old,df_new,alles_df,events,df_soil_output,n,plot=True):
+    
+    teliko=[]
+    telos=pd.DataFrame()
+    for p in tqdm(range(alles_df.shape[0])[:n]):
+#         print(f'Grammh apo frame{p}')
+        dates = pd.DataFrame(index=range(len(df_new)))
+        dates = dates.set_index(df_new.index)
+        dates['power']=df_new['power']        
+        dates['soil']=df_new.soiling_derate
+        stumpy.config.STUMPY_EXCL_ZONE_DENOM=alles_df.iloc[p]['exclusion_zone']
+        mp,mpi=stumpy.mstump(df_old[col], m=int(alles_df.iloc[p]['profile_wind']),normalize=True)
+        md,mi,_,_=stumpy.mmotifs(df_old[col], mp, mpi,min_neighbors=alles_df.iloc[p]['min_nei'],
+                                 max_distance=alles_df.iloc[p]['max_d'],cutoffs=alles_df.iloc[p]['cutoff'],
+                                 max_matches=alles_df.iloc[p]['max_m'],max_motifs=alles_df.iloc[p]['max_motif'])  
+        md,mi=clean_motifs(md,mi)
+        m=int(alles_df.iloc[p]['profile_wind'])
+#         print(f'Window: {m}')
+#         print(f'Motif index:{mi}')
+#         print(f'Motif Distance:{md}')
+        oliko_tupou=[]
+        df_ok=pd.DataFrame()
+        for typ in range(len(mi)):
+#             print(f'Type of Motif {typ}')
+            Oliko_query_score=[]
+            df_preall=pd.DataFrame()
+            
+            for d,i in enumerate(mi[typ]):
+#                 print(f'The index that motif start:{i}')
+                if md[typ][d]<1000.0:
+#                     print('pame')
+#                     print(md[typ][d])
+#                 print(f'Index:{mi[typ][i]}')
+                    query=df_old.power[i:i+m].values
+#                     print(f'Query: {query}')
+                    x=random.choice([np.inf,1,2,3,4,5,6,7,8,9,10])
+                    stumpy.config.STUMPY_EXCL_ZONE_DENOM = x
+                    out=stumpy.match(query, df_new.power,max_matches=events)
+#                     print("OUT")
+                    test=pd.DataFrame()
+                    test.index=df_new.index
+                    test['pred']=np.nan
+                    for k in out[:,1]:
+                        test['pred'].iloc[k:k+m]=1
+                    test['pred'] = test['pred'].fillna(0)
+                    test["actual"] = np.nan
+                    for start, end in zip(df_soil_output.SoilStart, df_soil_output.SoilStop):
+                        test.loc[start:end, 'actual'] = 1
+                    test['actual'] = test['actual'].fillna(0)
+                    f1,ps,recal,hamming,jaccard,cohen,roc=scorer(test)
+                    Oliko_query_score.append(f1)
+                    data={'profile_wind':m,
+                          'exclusion_zone':x,
+                          'motif_index': i,
+                          'motif_type': typ+1,
+                          'actual_nei':len(out[:,1]),
+                          'actual_motif':len(md),
+                          'recall':np.round(recal,5),
+                          'f1':np.round(f1,5),
+                          'precision':np.round(ps,5),
+                          'hamming':np.round(hamming,5),
+                          'jaccard':np.round(jaccard,5),
+                         'cohen':np.round(cohen,5),
+                         'roc':np.round(roc,5)}
+            
+                    df_for_each = pd.DataFrame(data,index=[0])
+                    df_preall=pd.concat([df_for_each,df_preall],axis=0)
+
+                    if plot==True:
+                        ax=df_new.soiling_derate.plot(color="green",label='Soil Derate',figsize=(20,10))
+                        for d in df_soil_output.SoilStart:
+                            ax.axvline(x=d, color='black', linestyle='-')
+    #                 ax.axvline(x=d, color='black', linestyle='-',label='Soil Start')
+                        for d in df_soil_output.SoilStop:
+                            ax.axvline(x=d, color='black', linestyle=':') 
+    #                 ax.axvline(x=d, color='black', linestyle=':',label='Soil Ends') 
+                        print(f'Neigh:{len(out[:,1])}')
+                        print(f'Score:{f1}')
+                        for k in out[:,1]:
+                            ax.axvspan(dates.index[k],dates.index[k+m], color = 'orange',ymax=0.96, alpha = 0.2)
+                        plt.show()
+
+#                 df_typ=pd.concat([df_typ,df_for_each],axis=0)
+            oliko_tupou.append(np.max(Oliko_query_score))
+
+            df_ok=pd.concat([df_ok,df_preall],axis=0)
+        teliko.append(np.max(oliko_tupou))
+
+        telos=pd.concat([telos,df_ok],axis=0)
+    telos=telos.reset_index(drop=True)
+    telos=telos.loc[telos[['f1']].drop_duplicates(['f1']).index]
+    telos=telos.sort_values(by=['f1'], ascending=False).reset_index(drop=True)
+                    
+                    
+            
+    return teliko,telos
+
+    
+    
+def match_graph_multi_dim_eval(col,df_new,df_old,df_soil_output,alles_df,n,plot=False):
+  
+    import sklearn.metrics
+    for p in range(alles_df.shape[0])[:n]:
+        index=int(alles_df.iloc[p]['motif_index'])
+        stumpy.config.STUMPY_EXCL_ZONE_DENOM=alles_df.iloc[p]['exclusion_zone']
+        m=int(alles_df.iloc[p]['profile_wind'])
+        dates = pd.DataFrame(index=range(len(df_new)))
+        dates = dates.set_index(df_new.index)
+        dates['power']=df_new['power']        
+        dates['soil']=df_new.soiling_derate
+        query=df_old.power[index:index+m]
+        out=stumpy.match(query, df_new.power,max_matches=len(df_soil_output))
+        test=pd.DataFrame()
+        test.index=df_new.index
+        test['pred']=np.nan
+        for k in out[:,1]:
+            test['pred'].iloc[k:k+m]=1
+        test['pred'] = test['pred'].fillna(0)
+        test["actual"] = np.nan
+        for start, end in zip(df_soil_output.SoilStart, df_soil_output.SoilStop):
+            test.loc[start:end, 'actual'] = 1
+        test['actual'] = test['actual'].fillna(0)
+        f1,ps,recal,hamming,jaccard,cohen,roc=scorer(test)
+        
+        if plot==True:
+            ax=df_new.soiling_derate.plot(color="green",label='Soil Derate',figsize=(20,10))
+            ax.set_xlabel('Periods defined by best matching ',fontsize=24)
+            ax.set_ylabel("Soil Derate",color="black",fontsize=24)
+            ax.set_title(f'{m}-Days Length, {len(out[:,1])}-Neighbors, F1:{np.round(f1,3)}',
+                                      fontsize=20)
+            for d in df_soil_output.SoilStart:
+                ax.axvline(x=d, color='black', linestyle='-')
+            ax.axvline(x=d, color='black', linestyle='-',label='Soil Start')
+            for d in df_soil_output.SoilStop:
+                ax.axvline(x=d, color='black', linestyle=':') 
+            ax.axvline(x=d, color='black', linestyle=':',label='Soil Ends') 
+            for k in out[:,1]:
+                ax.axvspan(dates.index[k],dates.index[k+m], color = 'orange',ymax=0.96, alpha = 0.9)
+            ax.axvspan(dates.index[k],dates.index[k+m], color = 'orange',ymax=0.96,label='Match' ,alpha = 0.9)
+
+            plt.legend(fontsize=14,loc='lower left')
+            plt.show()
+            print(sklearn.metrics.classification_report(test.actual,test.pred))
