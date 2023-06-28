ada58e4a07133380647cefbf5681bbd15761d79a
PanosGidarakos <74772930+PanosGidarakos@users.noreply.github.com>
2023-06-28 13:05:13 +0200

    Update learning.py

--- modules/learning.py
+++ modules/learning.py
@@ -6,6 +6,12 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn import metrics
 from modules.preprocessing import enumerate2
+from modules.io import *
+from modules.preprocessing import *
+from tqdm import tqdm
+import random
+import stumpy
+
 
 def predict(df_test, model, feats, target):
     """
@@ -221,3 +227,240 @@ def changepoint_scores(df, feats, target, d1, d2, w_train, w_val, w_test):
     else:
         raise Exception("Either the training set is empty or the test set is empty")
 
+
+
+
+def procedure(df,df_soil_output,pop_size,days,
+              num_generations,num_parents_mating,num_mutations,
+              col,events,parenting,crossover,mix_up=True):
+    """
+    The whole genetic algortihm procedure. Returns best outputs of fitness,
+    the last survived population,end_df: the frame created by all iterations,
+    alles_df: the last df with the best results
+    df: pandas dataframe
+    df_soil_output: pandas dataframe of soiling events
+    col: columns of the dataframe to perform the routine
+    days: integer. shift in the index of soiling events by days
+    popsize: integer. Creates individuals
+    events: integer. Soiling periods
+    num_generations: number of generations
+    num_parents_mating: nubmer of parents to mate
+    num_mutations: how many chromosomes will mutate after crossover    
+    """
+  #    print(f'pop_size:{pop_size}')
+#     print(f'num_gen:{num_generations}')
+
+    #Creating the initial population.
+    import warnings
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        import sklearn.metrics
+    start = time.time()
+
+    new_population=initilization_of_population_mp(pop_size,events)
+    # print(new_population)
+    # print(f'new_population:{new_population}')
+    # Number of the weights we are looking to optimize.
+    num_weights = len(new_population[0,:])
+    print(f'Features: {col}')
+    print(f'Chromosomes: {len(new_population[0,:])}')
+    print(f'Soiling Events: {events}')
+    print(f'Generations: {num_generations}')
+    print(f'Population :{len(new_population)}')
+    print(f'Parents: {num_parents_mating}')
+    
+#     print(f'num_weights: {num_weights}')
+    best_outputs = []
+    end_df=pd.DataFrame()
+    for generation in tqdm(range(num_generations)):
+        # Measuring the fitness of each chromosome in the population.
+        fitness,alles_df = fiteness_fun(df,df_soil_output,days,new_population,col)
+        result = [] 
+        for i in fitness: 
+            if i not in result: 
+                result.append(i)
+            else: 
+                result.append(0)
+        fitness=result
+#         print(generation,np.max(fitness))
+#         print(alles_df.head(1))
+        # Thei best result in the current iteration.  
+#         print(np.max(fitness))
+        if mix_up:
+            parenting=random.choice(['sss','ranks','randoms','tournament','rws'])
+#         print(parenting)
+        best_outputs.append(np.max(fitness))
+        # Selecting the best parents in the population for mating.
+        if parenting=='smp':
+            parents = select_mating_pool(new_population, fitness, 
+                                          num_parents_mating)
+        elif parenting=='sss':
+            parents = steady_state_selection(new_population,fitness, num_parents_mating)[0]
+        elif parenting=='ranks':
+            parents = rank_selection(new_population,fitness, num_parents_mating)[0]
+        elif parenting=='randoms':
+            parents = random_selection(new_population,fitness, num_parents_mating)[0]
+        elif parenting=='tournament':
+            parents = tournament_selection(new_population,fitness, num_parents_mating,toursize=100)[0]
+        elif parenting=='rws':
+            parents = roulette_wheel_selection(new_population,fitness, num_parents_mating)[0]
+        elif parenting=='sus':
+            parents = stochastic_universal_selection(new_population,fitness, num_parents_mating)[0]
+        else:
+            raise TypeError('Undefined parent selection type')
+        # Generating next generation using crossover.
+        offspring_size=(len(new_population)-len(parents), num_weights)
+        if mix_up:
+            crossover=random.choice(['single','twopoint','uni','scatter'])
+
+#         print(crossover)
+
+
+
+
+        if crossover=='single':
+            offspring_crossover=single_point_crossover(parents, offspring_size,crossover_probability=None)
+        elif crossover=='twopoint':
+            offspring_crossover=two_points_crossover(parents, offspring_size,crossover_probability=None)
+        elif crossover=='uni':
+            offspring_crossover=uniform_crossover(parents, offspring_size,crossover_probability=None)
+        elif crossover=='scatter':
+            offspring_crossover=scattered_crossover(parents, offspring_size,crossover_probability=None,num_genes=num_weights)
+        elif crossover=='old':
+            offspring_crossover = crossover(parents,
+                                           offspring_size=(len(new_population)-len(parents), num_weights))
+        else:
+            raise TypeError('Undefined crossover selection type')
+
+        
+        # Adding some variations to the offspring using mutation.
+        offspring_mutation = mutation(offspring_crossover, num_mutations)
+        # Creating the new population based on the parents and offspring.
+        new_population[0:parents.shape[0], :] = parents
+        new_population[parents.shape[0]:, :] = offspring_mutation
+        end_df=pd.concat([alles_df,end_df],axis=0)
+
+    end = time.time()
+    print(f'Time to complete: {np.round(end - start,2)}seconds')
+    return new_population,best_outputs,end_df,alles_df
+
+
+
+
+def fiteness_fun(df,df_soil_output,days,new_population,col):
+    fitness_each_itter=[]
+    list_score_jac=[]
+    alles_df=pd.DataFrame()
+    
+    for row in (range(len(new_population))):
+
+        if new_population[:,5][row]<3:
+            new_population[:,5][row]=3
+        if new_population[:,4][row]<1:
+            new_population[:,4][row]=1
+            
+       
+            
+        conc=pd.DataFrame()
+        md,mi,excluzion_zone=pmc(df=df,new_population=new_population,row=row,col=col)
+        lista_jac_mtype=[]
+        for n,k in enumerate(range(len(mi))):
+            test=tester(df,new_population,row,df_soil_output,mi,k)
+            f1,ps,recal,hamming,jaccard,cohen,roc=scorer(test)
+            lista_jac_mtype.append(f1)
+            data={'min_nei':int(new_population[row,0]),
+                 'max_d':new_population[row,1],
+                 'cutoff':new_population[row,2],
+                 'max_m':int(new_population[row,3]),
+                 'max_motif':int(new_population[row,4]),
+                 'profile_wind':int(new_population[row,5]),
+                 'exclusion_zone':excluzion_zone,
+                 'motif_type': n+1,
+                 'actual_nei':len(mi[k]),
+                 'actual_motif':len(md),
+                 'recall':np.round(recal,5),
+                 'f1':np.round(f1,5),
+                  'precision':np.round(ps,5),
+                  'hamming':np.round(hamming,5),
+                  'jaccard':np.round(jaccard,5),
+                 'cohen':np.round(cohen,5),
+                 'roc':np.round(roc,5)}
+            
+             
+            conc=pd.concat([conc,pd.DataFrame(data,index=[0])],axis=0)
+        alles_df=pd.concat([conc,alles_df],axis=0)
+        list_score_jac.append(np.max(lista_jac_mtype))
+    alles_df=alles_df.loc[alles_df[['f1']].drop_duplicates(['f1']).index]
+    alles_df=alles_df.sort_values(by=['f1'], ascending=False).reset_index(drop=True)
+    return list_score_jac,alles_df
+
+    
+
+
+def clean_motifs(md,mi):
+    """
+    Cleaning found motifs from trivial motifs or dirty neighbors
+    md: motif distance
+    mi: motif indexes
+    """
+    outp=[]
+    for j in range(0,len(mi)):
+        outp.append(np.delete(mi[j], np.where(mi[j] == -1)))
+    mi=outp    
+    outp=[]
+    for j in range(0,len(md)):
+        outp.append(md[j][~np.isnan(md[j])])
+    md=outp
+    return md,mi
+
+def pmc(df,new_population,row,col):
+
+
+    """
+    pmc:profile,motif,cleaning
+    creates a pipeline calculation the profile the motifs and clean them for each individual
+    df:df: pandas dataframe
+    new_population: population of individuals
+    row: the index of each individual
+    col: columns of the dataframe to perform the routine
+    """
+
+    from stumpy import mstump
+    from stumpy import mmotifs
+    x=random.choice([np.inf,1,2,3,4,5,6,7,8])
+    stumpy.config.STUMPY_EXCL_ZONE_DENOM = x
+
+    mp,mpi=stumpy.mstump(df[col].to_numpy().transpose(), m=int(new_population[row][5]),discords=False,normalize=True)
+
+    md,mi,sub,mdl=stumpy.mmotifs(df[col].to_numpy().transpose(),mp,mpi,
+                             min_neighbors=int(new_population[row][0]),
+                             max_distance=new_population[row][1],cutoffs=new_population[row][2],
+                             max_matches=int(new_population[row][3]),max_motifs=int(new_population[row][4]))  
+#     print(stumpy.config.STUMPY_EXCL_ZONE_DENOM)
+    md,mi=clean_motifs(md,mi)
+    return md,mi,stumpy.config.STUMPY_EXCL_ZONE_DENOM
+
+
+def tester(df,new_population,row,df_soil_output,mi,k):
+    test=pd.DataFrame()
+    test.index=df.index
+    test['pred']=np.nan
+    for i in mi[k]:
+        test['pred'].iloc[i:i+int(new_population[row,5])]=1
+    test['pred'] = test['pred'].fillna(0)
+    test["actual"] = np.nan
+    for start, end in zip(df_soil_output.SoilStart, df_soil_output.SoilStop):
+        test.loc[start:end, 'actual'] = 1
+    test['actual'] = test['actual'].fillna(0)
+    return test
+
+def scorer(test):  
+    import sklearn.metrics
+    f1=sklearn.metrics.f1_score(test.actual, test.pred)
+    ps=sklearn.metrics.precision_score(test.actual, test.pred,zero_division=0)
+    recal=sklearn.metrics.recall_score(test.actual, test.pred)
+    hamming=sklearn.metrics.hamming_loss(test.actual, test.pred)
+    jaccard=sklearn.metrics.jaccard_score(test.actual, test.pred,zero_division=0)
+    cohen=sklearn.metrics.cohen_kappa_score(test.actual, test.pred)
+    roc=sklearn.metrics.roc_auc_score(test.actual, test.pred)
+    return f1,ps,recal,hamming,jaccard,cohen,roc
